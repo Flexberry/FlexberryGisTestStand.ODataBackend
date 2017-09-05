6d7e04dbe629ea636c31081e5a27e0d47bacafe9
Sokolov Andrey <s.andrey-2011@yandex.ru>
2017-09-05 17:38:23 +0500

    Create FlexberryGisTestStand beckend

---
+++ ODataBackend/App_Start/ODataConfig.cs
@@ -0,0 +1,82 @@
+﻿namespace IIS.FlexberryGisTestStand
+{
+    using System;
+    using System.Reflection;
+
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+   
+    using ICSSoft.STORMNET;
+    using ICSSoft.Services;
+
+    using IIS.Caseberry.Logging.Objects;
+
+    using Microsoft.Practices.Unity;
+
+    using NewPlatform.Flexberry;
+    using NewPlatform.Flexberry.ORM.ODataService;
+    using NewPlatform.Flexberry.ORM.ODataService.Extensions;
+    using NewPlatform.Flexberry.ORM.ODataService.Functions;
+    using NewPlatform.Flexberry.ORM.ODataService.Model;
+    using NewPlatform.Flexberry.Services;
+
+
+    /// <summary>
+    /// Configure OData Service.
+    /// </summary>
+    internal static class ODataConfig
+    {
+        /// <summary>
+        /// Configure OData by DataObjects assembly.
+        /// </summary>
+        /// <param name="config">Http configuration object.</param>
+        /// <param name="container">Unity container.</param>
+        public static void Configure(HttpConfiguration config, IUnityContainer container)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            if (container == null)
+            {
+                throw new ArgumentNullException(nameof(container));
+            }
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            // Use Unity as WebAPI dependency resolver
+            config.DependencyResolver = new UnityDependencyResolver(container);
+
+            // Create EDM model builder
+            var assemblies = new[]
+            {
+                Assembly.Load("FlexberryGisTestStand.Objects"),
+                typeof(ApplicationLog).Assembly,
+                typeof(UserSetting).Assembly,
+                typeof(FlexberryUserSetting).Assembly,
+                typeof(Lock).Assembly
+            };
+            var builder = new DefaultDataObjectEdmModelBuilder(assemblies);
+
+            // Map OData Service
+            var token = config.MapODataServiceDataObjectRoute(builder);
+
+            // Register OData event handlers.
+            token.Events.CallbackAfterCreate = CallbackAfterCreate;
+        }
+
+        private static void CallbackAfterCreate(DataObject dataObject)
+        {
+            // TODO: implement handler
+        }
+
+        private static string Test(QueryParameters queryParameters)
+        {
+            return "Hello world!";
+        }
+
+    }
+}
---
+++ ODataBackend/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿namespace IIS.FlexberryGisTestStand
+{
+    using System;
+    using System.Web;
+    using System.Web.Http;
+
+    using ICSSoft.STORMNET.Business;
+
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.Configuration;
+
+    /// <summary>
+    /// Global class which is used at application startup.
+    /// </summary>
+    public class Global : HttpApplication
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event arguments.</param>
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.LoadConfiguration();
+            container.RegisterInstance(DataServiceProvider.DataService);
+            GlobalConfiguration.Configure(configuration => ODataConfig.Configure(configuration, container));
+        }
+    }
+}
