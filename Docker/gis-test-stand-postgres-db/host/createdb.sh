2da3357e269504ae09511e565da822bb6ec4834a
akosinsky <akosinsky@ICS-KK12324>
2018-04-06 17:39:07 +0500

    Enable postgis

--- Docker/gis-test-stand-postgres-db/host/createdb.sh
+++ Docker/gis-test-stand-postgres-db/host/createdb.sh
@@ -4,6 +4,7 @@ cd /docker/host
 /docker-cmd.sh& 
 
 until psql -U postgres <create.sql; do echo "Wait...";sleep 2; done 
+until psql -U postgres -d stand <postgis_enable.sql; do echo "Wait...";sleep 5; done 
 until psql -U standuser -d stand <PostgreSql.create.sql; do echo "Wait...";sleep 5; done 
 
 /etc/init.d/postgresql stop;  while su -c psql postgres </dev/null >/dev/null 2>&1; do sleep 1; done; echo "postgresql stopped"
