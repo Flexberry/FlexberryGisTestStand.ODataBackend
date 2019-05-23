11e4bd9e3b17f640965ca7d8a5a1dfbe017ae8e5
kafnevod <kaf@ics.perm.ru>
2019-05-23 14:50:14 +0500

    Move SQL To Docker subdirectory to void wget copy from repository Add .autobuild

--- Docker/gis-test-stand-postgres-db/host/createdb.sh
+++ Docker/gis-test-stand-postgres-db/host/createdb.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /docker/host
 
-/docker-cmd.sh& 
+/docker-cmd.sh&
 
-until psql -U postgres <create.sql; do echo "Wait...";sleep 2; done 
-until psql -U postgres -d stand <postgis_enable.sql; do echo "Wait...";sleep 5; done 
-until psql -U standuser -d stand <create_tables.sql; do echo "Wait...";sleep 5; done 
-until psql -U standuser -d stand <create_geo.sql; do echo "Wait...";sleep 5; done 
-until psql -U standuser -d stand <import_water_line.sql; do echo "Wait...";sleep 5; done 
-until psql -U standuser -d stand <import_water_poly.sql; do echo "Wait...";sleep 5; done 
+until psql -U postgres <create.sql; do echo "Wait...";sleep 2; done
+until psql -U postgres -d stand <postgis_enable.sql; do echo "Wait...";sleep 5; done
+until psql -U standuser -d stand <./SQL/PostgreSql/create_tables.sql; do echo "Wait...";sleep 5; done
+until psql -U standuser -d stand <./SQL/PostgreSql/create_tables.sql; do echo "Wait...";sleep 5; done
+until psql -U standuser -d stand <./SQL/PostgreSql/import_water_line.sql; do echo "Wait...";sleep 5; done
+until psql -U standuser -d stand <./SQL/PostgreSql/import_water_poly.sql; do echo "Wait...";sleep 5; done
 
 /etc/init.d/postgresql stop;  while su -c psql postgres </dev/null >/dev/null 2>&1; do sleep 1; done; echo "postgresql stopped"
 
